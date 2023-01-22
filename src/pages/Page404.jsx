b81a3e6b9926a44b3b0b968c6ece0e78e438624f
Himanshu <kumarhimanshu2219@gmail.com>
2023-01-22 18:26:07 +0530

    4✅ : Page not found

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,7 +1,20 @@
+import React from "react";
+import {useNavigate} from 'react-router-dom'
+import Button from "../components/Button";
+
 const Page404 = () => {
+
+    const navigator = useNavigate()
+    const handleGoBack = (url) => {
+        navigator(url)
+    }
     return (
         <>
-            404 page
+            <div className="flex flex-col items-center justify-center h-96  mt-10">
+                <p className="text-9xl font-bold text-blue-600">404</p>
+                <p className="text-xl font-bold text-blue-600">Page Not Found</p>
+                <Button title='Go Back' bg='bg-blue-600' handleClick={()=>handleGoBack('/home')}/>
+            </div>
         </>
     )
 }
