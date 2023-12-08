8814ea6cdd76a6002d5eca02bc34f27a8604fde9
Himanshu <kumarhimanshu2219@gmail.com>
2023-12-08 18:07:35 +0530

    twitter project added

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -3,6 +3,7 @@ import google_clone from "../assets/google.png";
 import yt from "../assets/yt.png";
 import movie from "../assets/movie.png";
 import weather_app from "../assets/weather-app.png";
+import twitter_clone from "../assets/twitter-clone.png";
 
 export const projectsData = [
   {
@@ -12,6 +13,14 @@ export const projectsData = [
       "Build Full Stack e-commerce shopping app using using latest tech stack and has a fully responsive design",
     preview: "https://e-commerce-himanshu-0981.vercel.app/",
     github: "https://github.com/Himanshu-0981/e-commerce",
+  },
+  {
+    title: "Twitter / X clone ",
+    src: twitter_clone,
+    description:
+      "Created a full stack twitter / x clone using latest web technology with an immersive UI and user experience ",
+    preview: "",
+    github: "",
   },
   {
     title: "Google Search Engine Clone",
@@ -43,12 +52,5 @@ export const projectsData = [
       "Build a sleek Weather App for real-time weather updates and forecasts.",
     preview: "https://dynamic-weather-app-murex.vercel.app/",
     github: "https://github.com/Himanshu-0981/dynamic-weather-app",
-  },
-  {
-    title: "N/A",
-    src: "",
-    description: "N/A",
-    preview: "",
-    github: "",
   },
 ];
